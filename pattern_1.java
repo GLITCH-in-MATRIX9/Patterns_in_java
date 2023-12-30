0f2e1db7d923f626a504d8e725e2fe56b9e044c4
Anjali Dass <98489478+GLITCH-in-MATRIX9@users.noreply.github.com>
2023-12-30 23:52:10 +0530

    Update pattern_1.java

--- pattern_1.java
+++ pattern_1.java
@@ -7,7 +7,7 @@ public class pattern_1{
         System.out.println("enter a number");
         int n=sc.nextInt();
         for (int i=0;i<n;i++){
-            for(int j=1;j<=i;j++){
+            for(int j=0;j<=i;j++){
                 System.out.print("*");// the only print statement will print these stars in the same line
             
             }
